d38e647cb2f2a0ad895dfd238523fc1690c17964
Michael Born <mborn319@gmail.com>
2018-09-20 23:59:30 -0400

    Got basic /products/list working.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Etsy On Bolt CMS
+
+## Setup
+
+* `composer install` in the root of the project.
+* Save `.env.dist` as `.env`
+* In `.env`, update the `ETSY_API_KEY` to your [Etsy API key][1] and `ETSY_SHOP_NAME` to your Etsy shop name. 
+
+[1]: https://www.etsy.com/developers/your-apps
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,13 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
 
 class ProductsController
 {
   public function list()
   {
-    return new Response(
-      '<html><body>TEST</body></html>'
-    );
+
+    $client = new Client([
+      'base_uri'  => 'https://openapi.etsy.com/',
+      'query'     => [ 'api_key' => getenv('ETSY_API_KEY') ]
+    ]);
+
+    $response = $client->request('GET',getenv('ETSY_API_VERSION') . '/shops/' . getenv('ETSY_SHOP_NAME') . '/listings/active');
+
+    if ( $response->getStatusCode() === "200" ) {
+      $error = 'ERROR ' . $response->getStatusCode(); 
+      return new Response(
+        '<p class="notification is-danger">' . $error. '</p>'
+      );
+    } else {
+      return new Response(
+        $response->getBody()->getContents()
+      );
+    }
   }
 }
